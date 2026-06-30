4942b5cca4a283456b612a4e465f2a684a3c577b
Alexmwale <alexmwalec03@gmail.com>
2026-06-30 20:55:17 +0200

    Updating origin URL

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin:'https://mayeso-lazalo-academic-researcher.vercel.app/',
+    origin:'https://mayeso-lazalo-academic-researcher.vercel.app',
   })
 );
 
